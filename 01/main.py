1ca59dee9939f1d7ab8017103c637243929b7189
Nath <nathan@nathanbailey.dev>
2023-12-02 16:06:48 +0000

    Day 1, part two solution

--- 01/main.py
+++ 01/main.py
@@ -1,11 +1,26 @@
 import re
 
+def numStringToValue(v: str):
+  switcher = {
+    "zero": 0,
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9
+  }
+  return str(switcher.get(v, v))
+
 with open("01/data/input.txt", "r") as file:
   data = list(map(lambda item: item.strip("\n"), file.readlines()))
 
 total = 0
 for i, item in enumerate(data):
-  nums = re.findall(r'\d', item)
+  nums = list(map(lambda res: numStringToValue(res), re.findall(r'\d|zero|one|two|three|four|five|six|seven|eight|nine', item)))
   num = nums[0] + nums[len(nums) - 1]
   total += int(num)
 
